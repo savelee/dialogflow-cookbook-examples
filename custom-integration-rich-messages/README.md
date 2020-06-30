daaa6d78b6bb812d7b8dfd7558cf118ad97275f1
Lee Boonstra <lee@leeboonstra.com>
2020-06-30 15:59:00 +0200

    rich cards in custom integration example

---
+++ custom-integration-rich-messages/README.md
@@ -0,0 +1,19 @@
+# Dialogflow custom integration
+
+**by: Lee Boonstra, Developer Advocate Conversational AI, Google Cloud**
+
+1. 'gcloud services enable dialogflow.googleapis.com'
+
+1. 'gcloud init' to login and get the right permissions.
+
+1. cd back-end
+
+2. Install the required libraries:
+
+    `npm install`
+
+3. Start the node app:
+
+   `npm --PROJECT_ID=[your-gcp-project-id] run start`
+
+4. Browse to http://localhost:3000
--- custom-integration-rich-messages/back-end/app.js
+++ custom-integration-rich-messages/back-end/app.js
@@ -1,59 +1,45 @@
-//1) These system variables can be set from the command-line with --PROJECT_ID, --PORT and --LANGUAGE
+const structJson = require('../back-end/structToJson');
+
 const projectId = process.env.npm_config_PROJECT_ID;
 const port = ( process.env.npm_config_PORT || 3000 );
 const languageCode = (process.env.npm_config_LANGUAGE || 'en-US');
 
-//2) Load all the libraries needed by this app
 const socketIo = require('socket.io');
-const http = require('http');
 const cors = require('cors');
 const express = require('express');
 const path = require('path');
-// These are specific to Dialogflow
 const uuid = require('uuid');
 const df = require('dialogflow').v2beta1;
 
-//3) Create an express app
 const app = express();
 
-//4) Setup Express, and load the static files and HTML page
 app.use(cors());
 app.use(express.static(__dirname + '/../ui/'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../ui/index.html'));
 });
 
-//5) Create the Server and listen to the PORT variable
+
 server = http.createServer(app);
 io = socketIo(server);
 server.listen(port, () => {
     console.log('Running server on port %s', port);
 });
 
-//6 Socket.io listener, once the client connect to the server socket
-// then execute this block.
 io.on('connect', (client) => {
     console.log(`Client connected [id=${client.id}]`);
     client.emit('server_setup', `Server connected [id=${client.id}]`);
 
-
-    // 7B When the client sends 'welcome' events
-    // then execute this block
     client.on('welcome', async function() {
+        console.log("1");
         const welcomeResults = await detectIntentByEventName('welcome');
-        client.emit('returnResults', welcomeResults);
+        client.emit('returnResults', [welcomeResults]);
     });
 
-    //7) When the client sends 'message' events
-    // then execute this block
     client.on('message', async function(msg) {
-        //console.log(msg);
-
-        //8) A promise to do intent matching
+        console.log(msg);
         const results = await detectIntent(msg);
-        console.log(results);
-
-        //9) Return the Dialogflow after intent matching to the client UI.
+        //console.log(results);
         client.emit('returnResults', results);
     });
 });
@@ -62,19 +48,11 @@ io.on('connect', (client) => {
  * Setup Dialogflow Integration
  */
 function setupDialogflow(){
-    //10) Dialogflow will need a session Id
     sessionId = uuid.v4();
-    
-    //11) Dialogflow will need a DF Session Client
-    // So each DF session is unique
     sessionClient = new df.SessionsClient();
-    
-    //12) Create a session path from the Session client, 
-    // which is a combination of the projectId and sessionId.
+ 
     sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-
-    //13) These objects are in the Dialogflow request
     request = {
       session: sessionPath,
       queryInput: {}
@@ -87,18 +65,18 @@ function setupDialogflow(){
   * @return response promise
   */
  async function detectIntent(text){
-    //14) Get the user utterance from the UI
     request.queryInput.text =  {
         languageCode: languageCode,
         text: text
     };
-    console.log(request);
     
-    //15) The Dialogflow SDK method for intent detection.
-    // It returns a Promise, which will be resolved once the
-    // fulfillment data comes in.
     const responses = await sessionClient.detectIntent(request);
-    return responses;
+
+    let data = getRichContent(responses);
+
+    console.log(data);
+
+    return data;
  }
 
 
@@ -107,17 +85,32 @@ function setupDialogflow(){
   * @param eventName - string
   * @return response promise
   */
- //16 
  async function detectIntentByEventName(eventName){
     request.queryInput.event =  {
         languageCode: languageCode,
         name: eventName
     };
     const responses = await sessionClient.detectIntent(request);
-    //remove the event, so the welcome event wont be triggered again
     delete request.queryInput.event;
-    return responses;
+    return responses[0].queryResult.fulfillmentText;
  }
 
- //Run this code.
+ function getRichContent(responses){
+    const result = responses[0].queryResult;
+    let messages = [];
+
+    if(result.fulfillmentMessages.length > 0) {
+        for (let index = 0; index < result.fulfillmentMessages.length; index++) {
+            const msg = result.fulfillmentMessages[index];
+            if (msg.payload){
+                let data = structJson.structProtoToJson(msg.payload);
+                messages.push(data.web);
+            } else {
+                messages.push(msg.text.text);
+            }
+        }
+        return messages;
+    }
+ }
+
  setupDialogflow();

5d8debfd0ee0bd2905bdce5a4fbfa19be826a8e5
Lee Boonstra <lee@leeboonstra.com>
2020-07-15 18:33:38 +0200

    cloud run verison of the multilang script

--- localized-webhook/index.js
+++ localized-webhook/index.js
@@ -1,7 +1,8 @@
 'use strict';
- 
-const functions = require('firebase-functions');
-const { WebhookClient, Card, Suggestion } = require('dialogflow-fulfillment');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const { WebhookClient} = require('dialogflow-fulfillment');
 
 //1
 const i18n = require('i18n');
@@ -16,45 +17,65 @@ i18n.configure({
 });
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
- 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-  const agent = new WebhookClient({ request, response });
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
- 
 
-  //3
-  //i18n.setLocale(conv.user.locale);
-  //moment.locale(conv.user.locale);
+//3
+function welcome(agent) {
+  agent.add(i18n.__('WELCOME_BASIC'));
+}
 
-  function welcome(agent) {
-    agent.add(i18n.__('WELCOME_BASIC'));
-  }
- 
-  function fallback(agent) {
-    agent.add(i18n.__('FALLBACK_BASIC'));
-  }
+function fallback(agent) {
+  agent.add(i18n.__('FALLBACK_BASIC'));
+}
 
-  function getPrice(agent) {
-    agent.add(i18n.__('PRICE'));
-  }
+function getPrice(agent) {
+  agent.add(i18n.__('PRICE', numeral(1000).format('399')));
+}
 
-  function getDeliveryDate(agent) {
-    agent.add(i18n.__('DELIVERY_DATE', moment().format('LL')));
-  }
+function getDeliveryDate(agent) {
+  agent.add(i18n.__('DELIVERY_DATE', moment().format('LL')));
+}
 
-  function getTotalNumber(agent) {
-    agent.add(i18n.__('TOTAL NUMBER', numeral(1000).format('0,0')));
-  }
+function getTotalNumber(agent) {
+  agent.add(i18n.__('TOTAL_AMOUNT', numeral(1000).format('0,0')));
+}
 
-  // Run the proper function handler based on the matched Dialogflow intent name
-  let intentMap = new Map();
-  intentMap.set('Default Welcome Intent', welcome);
-  intentMap.set('Default Fallback Intent', fallback);
 
-  intentMap.set('Get_Price', getPrice);
-  intentMap.set('Get_Delivery_Date', getDeliveryDate);
-  intentMap.set('Get_Total_Number', getTotalNumber);
+// Express Code
+const app = express().use(bodyParser.json());
 
-  agent.handleRequest(intentMap);
+app.post('/fulfillment', (request, response) => {
+    const agent = new WebhookClient({ request, response });
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+    //4
+    var lang = request.body.queryResult.languageCode;
+    var langCode;
+    if(lang === "nl") langCode = "nl-NL";
+    if(lang === "en") langCode = "en-US";
+    i18n.setLocale(langCode);
+    moment.locale(langCode);
+    numeral.locale(lang);
+
+    //5
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+  
+    intentMap.set('Get_Price', getPrice);
+    intentMap.set('Get_Delivery_Date', getDeliveryDate);
+    intentMap.set('Get_Total_Number', getTotalNumber);
+    agent.handleRequest(intentMap);
 });
+
+
+app.get('/', (req, res) => {
+    res.send(`OK`);
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('Dialogflow Fulfillment listening on port', port);
+});
+
+
